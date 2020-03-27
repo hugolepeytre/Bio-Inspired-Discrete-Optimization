1d00ecb625550b8e9f13ac4c476c9845f43654ab
hugolepeytre <hugo.lepeytre@gmail.com>
2020-03-27 10:32:54 +0100

    Swarm on the way, will try something with the ants

--- particle_swarm/src/particles.rs
+++ particle_swarm/src/particles.rs
@@ -1,28 +1,45 @@
 use crate::job_list::Ordering;
 
+use std::usize::MAX;
+
 pub struct Swarm {
     particles : Vec<Particle>,
+    best_known_pos : Vec<f32>,
+    best_known_time : usize,
 }
 
 impl Swarm {
     pub fn random(size : usize) -> Swarm {
         let particles = (0..size).map(|_| Particle::random()).collect();
-        return Swarm {particles}
+        let s = Swarm {particles, best_known_pos : Vec::new(), best_known_time : MAX};
+        return s
     }
 
     pub fn step(&mut self) {
         // TODO : Implement
+        // Make every particle do a step
+        // Evaluate them
     }
 
-    pub fn make_solutions<'a>(&self) -> Vec<Ordering<'a>> {
-        // TODO : Implement
-        return Vec::new()
+    fn evaluate(part : &Particle) {
+
+    }
+
+    pub fn best_time(&self) -> usize {
+        return self.best_known_time
+    }
+
+    pub fn best_solution<'a>(&self) -> Option<Ordering<'a>> {
+        // TODO : have it return something
+        None
     }
 }
 
 pub struct Particle {
     speed : f32,
     pos : Vec<f32>,
+    self_best_pos : Vec<f32>,
+    self_best_time : usize,
 }
 
 impl Particle {
@@ -33,5 +50,10 @@ impl Particle {
     pub fn random() -> Particle {
         // TODO : Implement
         return Self::new(0.0, Vec::new())
+    }
+
+    pub fn eval(&self) -> usize {
+        // TODO : Implement
+        return 0
     }
 }
--- particle_swarm/src/swarm.rs
+++ particle_swarm/src/swarm.rs
@@ -5,19 +5,10 @@ use crate::job_list::{Jobs, Ordering};
 use crate::particles::Swarm;
 
 pub fn run<'a>(jobs : &'a Jobs) -> Ordering<'a> {
-    let mut best_solution : Ordering = Ordering::random(&jobs);
-    let mut best_time = best_solution.end_time();
     let mut swarm : Swarm = Swarm::random(PARTICLES);
     for i in 0..GENERATIONS {
         swarm.step();
-        let solutions = swarm.make_solutions();
-        for sol in solutions {
-            if sol.end_time() < best_time {
-                best_time = sol.end_time();
-                best_solution = sol;
-            }
-        }
-        println!("Gen {} : {}", i + 1, best_time);
+        println!("Gen {} : {}", i + 1, swarm.best_time());
     }
-    return best_solution;
+    return swarm.best_solution().unwrap();
 }
